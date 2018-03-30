f479e8dd1bcc91bf13665a1e98f9e1161deed7e4
yobbo <yobbo_wang@163.com>
2018-03-30 12:24:32 +0800

    'update'

--- commit.bat
+++ commit.bat
@@ -3,8 +3,7 @@ rem commit git
 git add ./src/*
 git add ./pom.xml
 git add ./commit.bat
-git add ./bootstrap-alert.zip
-git add ./lib.zip
+git add ./*.zip
 git add ./README.md
 git remote add origin https://github.com/yobbo-wang/Nextrobot.git
 git commit -m 'update'
